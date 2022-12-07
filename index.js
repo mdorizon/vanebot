57faf0b8cc65acfd11d483491823ef5fadd5d48c
BuildTools <mdorizon17@gmail.com>
2022-12-08 00:10:26 +0100

    connected the bot to a mongodb cluster

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { Client, Collection } = require('discord.js');
 const dotenv = require('dotenv'); dotenv.config();
+const mongoose = require('mongoose')
 const client = new Client({ intents: 513 });
+
+mongoose.set('strictQuery', true);
 
 client.commands = new Collection();
 
@@ -10,5 +13,14 @@ process.on('exit', code => { console.log(`Le processus s'est arrêté avec le co
 process.on('uncaughtException', (err, origin) => { console.log(`UNCAUGHT_EXCEPTION: ${err}`, `Origine: ${origin}`) });
 process.on('unhandledRejection', (reason, promise) => { console.log(`UNHANDLED_REJECTION: ${reason}\n-----\n`, promise) });
 process.on('warning', (...args) => console.log(...args));
+
+mongoose.connect(process.env.DATABASE_URI, {
+    autoIndex: false,
+    maxPoolSize: 10,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+    family: 4
+}).then(() => { console.log('Le client est connecté à la base de données!') })
+.catch(err => { console.log(err); });
 
 client.login(process.env.DISCORD_TOKEN);
