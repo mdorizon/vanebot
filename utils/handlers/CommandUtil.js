22febdd535f89f1f2be4f073e49c4aced2469ced
BuildTools <mdorizon17@gmail.com>
2022-12-07 23:16:35 +0100

    tri command name

--- utils/handlers/CommandUtil.js
+++ utils/handlers/CommandUtil.js
@@ -5,6 +5,9 @@ const pGlob = promisify(glob);
 module.exports = async client => {
     (await pGlob(`${process.cwd()}/commands/*/*.js`)).map(async cmdFile => {
         const cmd = require(cmdFile);
+
+        if (!cmd.name) return console.log(`----\nCommande non-chargée: pas de nom\nFichier -> ${cmdFile}\n----`)
+
         client.commands.set(cmd.name, cmd)
         console.log(`Commande chargée: ${cmd.name}`);
     });
