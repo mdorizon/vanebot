0f51327dc5b10cade69105c041a821acb74506d1
BuildTools <mdorizon17@gmail.com>
2022-12-07 23:39:07 +0100

    slash command handler

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -1,6 +1,10 @@
 module.exports = {
     name: 'ping',
+    description: 'Command ping!',
     run: (client, message, args) => {
         message.channel.send('Pong!');
+    },
+    runSlash: (client, interaction) => {
+        interaction.reply('Pong!');
     },
 };
--- events/client/ready.js
+++ events/client/ready.js
@@ -1,7 +1,10 @@
 module.exports = {
     name: 'ready',
     once: true,
-    execute(client) {
+    async execute(client) {
         console.log('Je suis prêt!')
+
+        const devGuild = await client.guilds.cache.get('969556385174020117');
+        devGuild.commands.set(client.commands.map(cmd => cmd));
     },
 };
