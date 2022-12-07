62b4b6bbbc599cd708af0f1ba05420a5b2caec8a
BuildTools <mdorizon17@gmail.com>
2022-12-08 00:53:41 +0100

    added the basic guildmemberadd / remove events

--- events/guild_members/guildMemberRemove.js
+++ events/guild_members/guildMemberRemove.js
@@ -2,23 +2,26 @@ const dayjs = require('dayjs');
 const { MessageEmbed, Formatters } = require('discord.js');
 
 module.exports = {
-    name: 'guildMemberAdd',
+    name: 'guildMemberRemove',
     once: false,
     async execute(client, member) {
         const creationTimestamp = Formatters.time(dayjs(member.user.createdTimestamp).unix(), Formatters.TimestampStyles.ShortDateTime);
         const relativeCreationTimestamp = Formatters.time(dayjs(member.user.createdTimestamp).unix(), Formatters.TimestampStyles.RelativeTime);
         const joinTimestamp = Formatters.time(dayjs(member.joinedTimestamp).unix(), Formatters.TimestampStyles.ShortDateTime);
         const relativeJoinTimestamp = Formatters.time(dayjs(member.joinedTimestamp).unix(), Formatters.TimestampStyles.RelativeTime);
+        const leaveTimestamp = Formatters.time(dayjs().unix(), Formatters.TimestampStyles.ShortDateTime);
+        const relativeLeaveTimestamp = Formatters.time(dayjs().unix(), Formatters.TimestampStyles.RelativeTime);
 
         const embed = new MessageEmbed()
             .setAuthor({ name: `${member.user.tag} (${member.id})`, iconURL: member.displayAvatarURL() })
-            .setColor('#21ff81')
-            .setDescription(`± Nom d'utilisateur: ${member}
+            .setColor('#dc143c')
+            .setDescription(`± Nom d'utilisateur: ${member.displayName}
             ± Crée le: ${creationTimestamp} (${relativeCreationTimestamp})
             ± Rejoint le: ${joinTimestamp} (${relativeJoinTimestamp})
+            ± Quitté le: ${leaveTimestamp} (${relativeLeaveTimestamp})
             `)
             .setTimestamp()
-            .setFooter({ text: 'L\'utilisateur a rejoint!' });
+            .setFooter({ text: 'L\'utilisateur a quitté!' });
 
         const logChannel = client.channels.cache.get('1050140471491580097');
         logChannel.send({ embeds: [embed] });
