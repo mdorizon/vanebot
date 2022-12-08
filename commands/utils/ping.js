97cf36dabf3a1c3bafac242809a64706b84be9dd
BuildTools <mdorizon17@gmail.com>
2022-12-08 11:04:59 +0100

    edit message and interaction

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -3,30 +3,36 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
     name: 'ping',
     description: 'Command ping!',
-    run (client, message, args) {
+    async run (client, message, args) {
+        const tryPong = await message.channel.send("On essaye de pong... un instant!");
+
         const embed = new MessageEmbed()
             .setTitle('🏓 Pong!')
             .setThumbnail(client.user.displayAvatarURL())
             .addFields(
-                { name: 'Latence', value: `\`${client.ws.ping}ms\``, inline: true },
+                { name: 'Latence API', value: `\`\`\`${client.ws.ping}ms\`\`\``, inline: true },
+                { name: 'Latence BOT', value: `\`\`\`${tryPong.createdTimestamp - message.createdTimestamp}ms\`\`\``, inline: true },
                 { name: 'Uptime', value: `<t:${parseInt(client.readyTimestamp / 1000)}:R>`, inline: true }
             )
             .setTimestamp()
             .setFooter({ text: message.author.username, iconURL: message.author.displayAvatarURL() });
 
-        message.channel.send({ embeds: [embed] });
+        tryPong.edit({ content: ' ', embeds: [embed] });
     },
-    runInteraction (client, interaction) {
+    async runInteraction (client, interaction) {
+        const tryPong = await interaction.reply({ content: "On essaye de pong... un instant!", fetchReply: true });
+
         const embed = new MessageEmbed()
             .setTitle('🏓 Pong!')
             .setThumbnail(client.user.displayAvatarURL())
             .addFields(
-                { name: 'Latence', value: `\`${client.ws.ping}ms\``, inline: true },
+                { name: 'Latence API', value: `\`\`\`${client.ws.ping}ms\`\`\``, inline: true },
+                { name: 'Latence BOT', value: `\`\`\`${tryPong.createdTimestamp - interaction.createdTimestamp}ms\`\`\``, inline: true },
                 { name: 'Uptime', value: `<t:${parseInt(client.readyTimestamp / 1000)}:R>`, inline: true }
             )
             .setTimestamp()
             .setFooter({ text: interaction.user.username, iconURL: interaction.user.displayAvatarURL() });
 
-        interaction.reply({ embeds: [embed] });
+        interaction.editReply({ content: ' ', embeds: [embed] });
     },
 };
