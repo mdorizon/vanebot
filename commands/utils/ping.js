5d92b65e294e6d251f648ed64bab150adf190537
BuildTools <mdorizon17@gmail.com>
2022-12-08 11:40:34 +0100

    commands permissions start

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'ping',
+    permissions: ['ADMINISTRATOR'],
     description: 'Command ping!',
     async run (client, message, args) {
         const tryPong = await message.channel.send("On essaye de pong... un instant!");
