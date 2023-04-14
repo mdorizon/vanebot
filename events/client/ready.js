f69428f2cfc189d44623a5932a448b0f63f97a4b
BuildTools <mdorizon17@gmail.com>
2023-04-14 15:41:27 +0200

    modification of the ready.js membercount

--- events/client/ready.js
+++ events/client/ready.js
@@ -10,15 +10,7 @@ module.exports = {
         const devGuild = await client.guilds.cache.get('969556385174020117');
         devGuild.commands.set(client.commands.map(cmd => cmd));
 
-        let memberct;
-
-        if (devGuild.memberCount == 50) {
-            memberct = '49.3'
-        } else {
-            memberct = devGuild.memberCount
-        }
-
-        timer=setInterval(function() { memberChannel.setName(`〢📊᲼membres : ${memberct}`); }, 300000);
+        timer=setInterval(function() { memberChannel.setName(`〢📊᲼membres : ${devGuild.memberCount}`); }, 300000);
 
     },
 };
